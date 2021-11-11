4ad878ccde0302e3b6cf76b5f02cfd9d67f9beab
davidcoats <david@getsafetycone.com>
2021-11-11 12:35:56 -0800

    Initial commit of source-stub.

---
+++ source/R5T.T0086.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0086.Construction
+{
+    /// <summary>
+    /// Construction project for ILogger and ILoggerOperator extension method bases..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0086/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0086
+{
+    /// <summary>
+    /// ILogger and ILoggerOperator extension method bases.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
